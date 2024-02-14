65eb6c9f82429dfdfbbcb23a6f0b9f65793c6e32
Azro1 <azro1.development@gmail.com>
2024-02-14 18:59:44 +0000

    Set up POST request handler and tested it by creating a new project in postman and making a post request to it. It takes in a request object which is essentially the request body and we use the json method on that to get access to the data. From there we make a post to the local json file sending the data and recieve it back once request is completed successfully. Then send it back to client.

--- app/api/projects/route.js
+++ app/api/projects/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server"
 
-// forces handlers to use dynamic rendering
+// forces handlers to be dynamic during development and dynamically built at buid time
 export const dynamic = 'force dynamic'
 
 // get
@@ -12,3 +12,27 @@ export async function GET() {
     status: 200
   })
 }
+
+// post
+export async function POST(request) {
+    // first make new post to this handler from postman to add a new project which we recieve from this request param and store in const
+    const project = await request.json()
+  
+    // send it to json file in post request
+    const res = await fetch('http://localhost:8080/projects', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(project)
+    })
+  
+    // post returns it
+    const newProject = await res.json()
+  
+    // sent it back to postman
+    return NextResponse.json(newProject, {
+      status: 201
+    })
+  }
+  
