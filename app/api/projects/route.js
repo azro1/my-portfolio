0219e3aa0b4f17e172cb5ab2c54e126e497e47fb
Azro1 <azro1.development@gmail.com>
2024-02-14 18:42:36 +0000

    Forced all of my route handlers to be dynamic during development and dynamically built at build time  overiding the default behaviour of next making static during development and statically building them at build time - caching the results of fetch requests

--- app/api/projects/route.js
+++ app/api/projects/route.js
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server"
 
+// forces handlers to use dynamic rendering
+export const dynamic = 'force dynamic'
+
 // get
 export async function GET() {
   const res = await fetch('http://localhost:8080/projects')
