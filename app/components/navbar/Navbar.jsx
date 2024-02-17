922ed93650b9e7df73473dda620c876345189e66
Azro1 <azro1.development@gmail.com>
2024-02-17 12:31:12 +0000

    Using flex justify-between on Navbar elements instead of mr-auto on logo

--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -26,8 +26,8 @@ const Navbar = ({ user }) => {
 
   return (
     <main>
-      <nav className='flex items-center h-9.5 relative gap-8'>
-        <Link href='/' className='mr-auto shadow-3xl p-3.5 rounded-xl bg-shade'>
+      <nav className='flex items-center justify-between h-9.5 relative'>
+        <Link href='/' className='shadow-3xl p-3.5 rounded-xl bg-shade'>
           <h2 className='logo font-eb text-hint'>
             Port<span>folio</span>
           </h2>
@@ -43,8 +43,10 @@ const Navbar = ({ user }) => {
             </div>
           )}
         </button>
+        
+        {user && <p className="text-hint absolute left-0 top-36 md:static">Hello, <span className="text-secondary">{user.email}</span></p>}
+
         <div className="flex items-center gap-16">
-            {user && <p className="text-hint absolute left-0 top-36 md:static">Hello, <span className="text-secondary">{user.email}</span></p>}
           <div className='links hidden lg:flex lg:items-center gap-12'>
             <Link href="/about">
               <div className="tooltip group">
