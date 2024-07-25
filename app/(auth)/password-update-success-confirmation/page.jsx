6c4c26bfef44a197c045b867ad747a406a3c72ed
azro1 <azro1.development@gmail.com>
2024-07-25 16:33:06 +0100

    Added new styles for LoggedInMenu, LoggedOutMenu & LayoutNav links. Added dynamic variable to root layout file to tell next to render dynamic pages for build test.

--- app/(auth)/password-update-success-confirmation/page.jsx
+++ app/(auth)/password-update-success-confirmation/page.jsx
@@ -5,7 +5,7 @@ const Confirmation = () => {
     <div className="flex flex-col items-center justify-center text-center h-auth-page-height">
       <h2 className='pb-3 text-3xl font-b text-accentRed'>Success!</h2>
       <p className='leading-7'>The reset has been successful and your password has been updated. </p>
-      <p className='leading-7'>Go back to the <Link className='text-base text-accentRed' href='/'>Homepage</Link>.</p>
+      <p className='leading-7'>Go to the <Link className='text-base text-accentRed' href='/'>Homepage</Link>.</p>
     </div>
   )
 }
--- app/components/navbar/LoggedOutMenu.jsx
+++ app/components/navbar/LoggedOutMenu.jsx
@@ -13,39 +13,39 @@ const LoggedOutMenu = ({ handleCloseMenu }) => {
   return (
     <>
       <Link href='/'>
-        <div className='flex items-center p-3 mb-1 bg-nightSky group hover:bg-frostWhite' onClick={handleCloseMenu}>
-          <FaHome className="text-accentRed group-hover:text-nightSky transition duration-300" size={18} />
-          <span className='text-stoneGray group-hover:text-nightSky text-base ml-3.5'>Home</span>
+        <div className='flex items-center p-3 mb-1 bg-nightSky group hover:bg-softCharcoal' onClick={handleCloseMenu}>
+          <FaHome className="text-accentRed" size={18} />
+          <span className='text-stoneGray group-hover:text-frostWhite transition duration-300 text-base ml-3.5'>Home</span>
         </div>
       </Link>
       <Link href='/about'>
-        <div className='flex items-center p-3 mb-1 bg-nightSky group hover:bg-frostWhite' onClick={handleCloseMenu}>
-          <FaInfoCircle className="text-accentRed group-hover:text-nightSky transition duration-300" size={17} />
-          <span className='text-stoneGray group-hover:text-nightSky text-base ml-3.5'>About</span>
+        <div className='flex items-center p-3 mb-1 bg-nightSky group hover:bg-softCharcoal' onClick={handleCloseMenu}>
+          <FaInfoCircle className="text-accentRed" size={17} />
+          <span className='text-stoneGray group-hover:text-frostWhite transition duration-300 text-base ml-3.5'>About</span>
         </div>
       </Link>
       <Link href='/contact'>
-        <div className='flex items-center p-3 mb-1 bg-nightSky group hover:bg-frostWhite' onClick={handleCloseMenu}>
-          <FaEnvelope className="text-accentRed group-hover:text-nightSky transition duration-300" size={16} />
-          <span className='text-stoneGray group-hover:text-nightSky text-base ml-3.5'>Contact</span>
+        <div className='flex items-center p-3 mb-1 bg-nightSky group hover:bg-softCharcoal' onClick={handleCloseMenu}>
+          <FaEnvelope className="text-accentRed" size={16} />
+          <span className='text-stoneGray group-hover:text-frostWhite transition duration-300 text-base ml-3.5'>Contact</span>
         </div>
       </Link>
       <Link href='/login'>
-        <div className='flex items-center p-3 mb-1 bg-nightSky group hover:bg-frostWhite' onClick={handleCloseMenu}>
-          <FaSignInAlt className="text-accentRed group-hover:text-nightSky transition duration-300" size={20} />
-          <span className='text-stoneGray group-hover:text-nightSky text-base ml-3.5'>Login</span>
+        <div className='flex items-center p-3 mb-1 bg-nightSky group hover:bg-softCharcoal' onClick={handleCloseMenu}>
+          <FaSignInAlt className="text-accentRed" size={20} />
+          <span className='text-stoneGray group-hover:text-frostWhite transition duration-300 text-base ml-3.5'>Login</span>
         </div>
       </Link>
       <Link href='/signup'>
-        <div className='flex items-center p-3 mb-1 bg-nightSky group hover:bg-frostWhite' onClick={handleCloseMenu}>
-          <FaUserPlus className="text-accentRed group-hover:text-nightSky transition duration-300" size={22} />
-          <span className='text-stoneGray group-hover:text-nightSky text-base ml-3.5'>Sign up</span>
+        <div className='flex items-center p-3 mb-1 bg-nightSky group hover:bg-softCharcoal' onClick={handleCloseMenu}>
+          <FaUserPlus className="text-accentRed" size={22} />
+          <span className='text-stoneGray group-hover:text-frostWhite transition duration-300 text-base ml-3.5'>Sign up</span>
         </div>
       </Link>
       <Link href='/help'>
-        <div className='flex items-center p-3 bg-nightSky group hover:bg-frostWhite' onClick={handleCloseMenu}>
-          <FaQuestionCircle className="text-accentRed group-hover:text-nightSky transition duration-300" size={18} />
-          <span className='text-stoneGray group-hover:text-nightSky text-base ml-3.5'>Help</span>
+        <div className='flex items-center p-3 bg-nightSky group hover:bg-softCharcoal' onClick={handleCloseMenu}>
+          <FaQuestionCircle className="text-accentRed" size={18} />
+          <span className='text-stoneGray group-hover:text-frostWhite transition duration-300 text-base ml-3.5'>Help</span>
         </div>
       </Link>
     </>
